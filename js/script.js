2a7675d7635f63a01a50222b7c7326580731b5c8
김이삭 <kimisaak0@gmail.com>
2021-03-02 16:55:27 +0900

    chapter3 완성본

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-document.getElementsByTagName('h1'); //h1 요소 선택
+//document.getElementsByTagName('h1'); //h1 요소 선택
 //문서에서 대상을 찾은 것이므로 document 클래스의 메소드를 사용. 인자로 문자열을 받으므로 ''나 ""로 묶어줌.
 //노드 : 문서상의 객체를 구분 짓는 하나하나의 덩어리.
 // '요소노드/element node', '속성노드/attribute node', '텍스트노드/text node' 등이 있다.
+
+var $h1 = jQuery('h1');
